1411b30a34c60c7f6ca16e05d5f894d30abe9f3e
user <user@Users-Computer-2.local>
2015-03-16 20:47:53 -0700

    datascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//add script here
+function tipCalculate(){
+
+	var totalBill = document.getElementById('bill').value;
+	var percBtns = document.getElementsByClassName('tip-btn');
+	console.log(percBtns);
+
+for(var i=0; i<percBtns.length; i++){
+	if(percBtns[i].checked){
+	var tipPerc = percBtns[i].value;
+	}
+}
+
+var tipTotal = totalBill * tipPerc; 
+var tipBox = document.getElementById('tipamount');
+tipBox.innerHTML = '$'+tipTotal;
+
+}
