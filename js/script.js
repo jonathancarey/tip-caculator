5177989bd127369d57be77e5d132d69d67927ca4
user <user@Users-Computer-2.local>
2015-03-23 19:25:05 -0700

    new branch

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 function tipCalculate(){
 
 	var totalBill = document.getElementById('bill').value;
-	var percBtns = document.getElementsByClassName('tip-btn');
+	
+var percBtns = document.getElementsByClassName('tip-btn');
 	console.log(percBtns);
 
 for(var i=0; i<percBtns.length; i++){
 	if(percBtns[i].checked){
 	var tipPerc = percBtns[i].value;
-	}
+}
+} 
+
+
+if(!isNaN(totalBill)){
+var tipTotal = Math.floor(totalBill * tipPerc); 
+var tipBox = document.getElementById('tipamount'); 
+	tipBox.innerHTML = '$'+tipTotal;}else {alert('numbers only'); resetCalc();
 }
 
-var tipTotal = totalBill * tipPerc; 
-var tipBox = document.getElementById('tipamount');
-tipBox.innerHTML = '$'+tipTotal;
+}
 
+function resetCalc(){
+document.getElementById('bill').value = '';
+
+var percBtns = document.getElementsByClassName('tip-btn');
+for(var i=0; i < percBtns.length; i++){
+ 	percBtns[i].checked = false;
+}
+
+ var tipBox = document.getElementById('tipamount');
+ 	tipBox.innerHTML = '$0.00';
 }
